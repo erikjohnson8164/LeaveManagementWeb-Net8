ffe1dc2f5ca5a41e694dc3c39c041dadb9d2032a
ejohnson-tp <ejohnson@davidjeremiah.org>
2024-05-04 22:21:26 -0700

    Added Database Tables and Migrations. End Implement Database Section

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveType LeaveType { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public int LeaveTypeId { get; set; }
+
+        public string EmployeeId { get; set; }
+    }
+}
